654a047e445d6a97021dce375316d3a2ef42a64a
Calculus <mikegrishchenko@gmail.com>
2022-04-15 12:38:48 -0700

    -m commit "debug"

---
+++ 1-HTML-CSS-JavaScript/practic.js
@@ -0,0 +1,15 @@
+if (age > 12) {
+    if (age < 65) {
+        ticketPrice = 20;
+    } else {
+        ticketPrice = 10;
+    }
+} else {
+    ticketPrice = 10;
+}
+
+if((age > 12) && (age < 65)) { 
+    ticketPrice = 20;
+} else {
+    ticketPrice = 10;
+}
