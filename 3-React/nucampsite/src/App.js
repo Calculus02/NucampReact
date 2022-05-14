acdbfd369e39ceaa75e75a4d6e032de548ed26ab
Calculus <mikegrishchenko@gmail.com>
2022-05-14 12:37:52 -0700

    Week 4 Workshop

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -6,17 +6,19 @@ import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
+import AboutPage from './pages/AboutPage';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
-            </Routes>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='contact' element={<ContactPage />} />
+                    <Route path='directory' element={<CampsitesDirectoryPage />} />
+                    <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+                    <Route path='about' element={<AboutPage />} />
+                </Routes>
             <Footer />
         </div>
     );
