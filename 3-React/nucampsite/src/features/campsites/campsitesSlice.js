694b6cbc8ea8aef20033fe84a0421c33c7287643
Calculus <mikegrishchenko@gmail.com>
2022-05-13 20:02:21 -0700

    CampsiteDetailPage with useParams

--- 3-React/nucampsite/src/features/campsites/campsitesSlice.js
+++ 3-React/nucampsite/src/features/campsites/campsitesSlice.js
@@ -8,8 +8,12 @@ export const selectAllCampsites = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 // };
 
+// export const selectCampsiteById = (id) => {
+//     return CAMPSITES.find((campsite) => campsite.id === id);
+// };
+
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
