db052183653da2160abf237b04f38b8a599093dd
Calculus <mikegrishchenko@gmail.com>
2022-06-03 16:46:21 -0700

    Async Partners

--- 3-React/nucampsite/src/features/partners/PartnersList.js
+++ 3-React/nucampsite/src/features/partners/PartnersList.js
@@ -2,23 +2,47 @@ import { Col, Row } from 'reactstrap';
 import Partner from './Partner';
 import { selectAllPartners } from './partnersSlice';
 import { useSelector } from 'react-redux';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 function PartnersList() {
     // const partners = selectAllPartners();
     const partners = useSelector(selectAllPartners);
+    
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
 
-    return (
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
         <Col className='mt-4'>
             <Row>
                 {partners.map((partner) => {
-                return (
-                    <div className='d-flex mb-5' key={partner.id}>
-                        <Partner partner={partner} />
-                    </div>
-                )})}
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
             </Row>
         </Col>
     );
 };
+
+//     return (
+//         <Col className='mt-4'>
+//             <Row>
+//                 {partners.map((partner) => {
+//                 return (
+//                     <div className='d-flex mb-5' key={partner.id}>
+//                         <Partner partner={partner} />
+//                     </div>
+//                 )})}
+//             </Row>
+//         </Col>
+//     );
+// };
 
 export default PartnersList;
