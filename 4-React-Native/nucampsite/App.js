583ec4876174786e83038ef7feeed7fce3298f76
Calculus <mikegrishchenko@gmail.com>
2022-06-17 20:26:42 -0700

    Navigation Part 1

--- 4-React-Native/nucampsite/App.js
+++ 4-React-Native/nucampsite/App.js
@@ -1,7 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './screens/MainComponent';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return <Main />;
+    return (
+        <NavigationContainer>
+            <Main />
+        </NavigationContainer>
+    );
 }
